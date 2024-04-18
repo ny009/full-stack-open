0df4cb6a0fe518c2bc2761ce5d9b106bfb756d9d
Nehal Yatham <nehal@nehals-mbp.mynetworksettings.com>
2024-04-17 21:43:09 -0400

    exercise1.4 - refactored part - made parts an array

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -1,31 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} 
-        part2={part2} 
-        part3={part3} 
-      />
-      <Total 
-        part1={part1} 
-        part2={part2} 
-        part3={part3}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -35,12 +29,12 @@ const Part = ({name, exercise}) => <p>{name} {exercise}</p>
 
 const Content = (props) => (
   <>
-    <Part name={props.part1.name} exercise={props.part1.exercises}/>
-    <Part name={props.part2.name} exercise={props.part2.exercises}/>
-    <Part name={props.part3.name} exercise={props.part3.exercises}/>
+    <Part name={props.parts[0].name} exercise={props.parts[0].exercises}/>
+    <Part name={props.parts[1].name} exercise={props.parts[1].exercises}/>
+    <Part name={props.parts[2].name} exercise={props.parts[2].exercises}/>
   </>
 )
 
-const Total = ({part1, part2, part3}) => <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+const Total = ({parts}) => <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
 
 export default App
